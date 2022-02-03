ab2fb9a636999f289ffeb13ce97ef70fa7a63aeb
nikhil <developerweb6@gmail.com>
2022-02-04 01:37:36 +0530

    register and password reset page

--- Pages/Register.js
+++ Pages/Register.js
@@ -2,10 +2,8 @@ import React from "react";
 import { TouchableOpacity, Image, ImageBackground, StyleSheet, TextInput, View, Text } from 'react-native';
  
 const image = { uri: "https://www.pngitem.com/pimgs/m/700-7009042_clip-art-freeuse-download-go-nurse-best-nursing.png" };
-export default function App() {
+export default function Register() {
   const [text, onChangeText] = React.useState(null);
-  const [email, onChangeEmail] = React.useState(null);
-  const [Password, onChangePassword] = React.useState(null);
   const [number, onChangeNumber] = React.useState(null);
   return (
     <View style={styles.container}>
@@ -16,19 +14,45 @@ export default function App() {
           source={require('./assets/logo.png')}
         />      
       <View style={{marginVertical:80}}></View>
-      <h3 style={styles.bodytext}>Reset Password</h3>
+      <h3 style={styles.bodytext}>Create your account</h3>
       <TextInput
         style={styles.input}
         onChangeText={onChangeText}
         value={text}
-        placeholderTextColor = "black"
-        placeholder="Enter Email"
+        placeholderTextColor = "white"
+        placeholder="Username"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholderTextColor = "white"
+        placeholder="Email"
+        keyboardType="numeric"
+      />
+       <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        secureTextEntry={true}
+        placeholderTextColor = "white"
+        placeholder="Password"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        secureTextEntry={true}
+        placeholderTextColor = "white"
+        placeholder="Confirm Password"
         keyboardType="numeric"
       />
        <TouchableOpacity
         style={styles.button}
       >
-        <Text style={{color:'white',fontSize: 26}}>Submit</Text>
+        <Text style={{color:'white',fontSize: 26}}>Register</Text>
       </TouchableOpacity>
       </View>
      <p></p>
@@ -62,6 +86,9 @@ const styles = {
     marginTop: 10,
     textAlign:'center',
   },
+  forget: {
+    textAlign:'right',
+  },
   input: {
     height: 56,
     marginVertical: 12,
@@ -69,13 +96,12 @@ const styles = {
     borderRadius: 30,
     fontSize: 26,
     textAlign:'center',
-    color: 'black' ,
-    backgroundColor:'#ffff',
+    color: 'white' ,
+    backgroundColor:'#cbc5c5d4',
     padding: 10,
   },
   overlay: {
       flex: 1,
-    padding:30,
-    backgroundColor:"#726f6f63"
+    padding:30
   }
 };
